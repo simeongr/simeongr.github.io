9ac18455377de08c741e624a37dc1c538c5f0a06
Simeon Georgiev <simeonge@gmail.com>
2023-02-08 15:53:11 +0200

    WIP

--- test.js
+++ test.js
@@ -6,11 +6,20 @@ function trimString(text, i) {
     return `${text.substring(0, 100)}<span id="dots${i}">...</span><span id="more${i}" style="display:none">${text.substring(100, text.length)}</span>`;
 }
 
+function showMoreButton(text, i) {
+    if (text.length > 100) {
+        return `<a onclick="showMore(this, 'dots${i}', 'more${i}')">Покажи още</a>`;
+    }
+
+    return ''
+}
+
 let checkboxes = `<fieldset>`
 for (var i = 0; i < 45; i++) {
     classText = trimString(classes[i], i);
+    showMoreButton = showMoreButton(classes[i], i)
     checkboxes += `
-    <div id="tmclass${i}" class="fusion-form-checkbox">
+    <div id="tmclass${i+1}" class="fusion-form-checkbox">
         <input
           tabindex=""
           id="checkbox-checkedclasses-1-${i}"
@@ -21,7 +30,7 @@ for (var i = 0; i < 45; i++) {
           data-holds-private-data="false"
         />
         <label for="checkbox-checkedclasses-1-${i}"> ${classText} </label>
-        <a onclick="showMore(this, 'dots${i}', 'more${i}')">Покажи още</a>
+        ${showMoreButton}
     </div>`
 }
 checkboxes+=`</fieldset>`
