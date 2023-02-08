2a681f441139f0100252144b469c262607807feb
Simeon Georgiev <simeonge@gmail.com>
2023-02-08 15:46:44 +0200

    Test

--- test.js
+++ test.js
@@ -1,5 +1,14 @@
+function trimString(text, i) {
+    if (text.length < 100) {
+        return text;
+    }
+
+    return `${text.substring(0, 100)}<span id="dots${i}">...</span><span id="more${i}" style="display:none">${text.substring(100, text.length)}</span>`;
+}
+
 let checkboxes = `<fieldset>`
 for (var i = 0; i < 45; i++) {
+    classText = trimString(classes[i], i);
     checkboxes += `
     <div id="tmclass${i}" class="fusion-form-checkbox">
         <input
@@ -11,9 +20,7 @@ for (var i = 0; i < 45; i++) {
           class="fusion-form-input"
           data-holds-private-data="false"
         />
-        <label for="checkbox-checkedclasses-1-${i}">
-            ${classes[i]}
-        </label>
+        <label for="checkbox-checkedclasses-1-${i}"> ${classText} </label>
         <a onclick="showMore(this, 'dots${i}', 'more${i}')">Покажи още</a>
     </div>`
 }
