a4535bb39a012bc29958322f15b1db8b9ea9c26f
Simeon Georgiev <simeonge@gmail.com>
2023-02-11 08:31:03 +0200

    WIP

---
+++ showItems.js
@@ -0,0 +1,22 @@
+let itemcheckboxes = `<fieldset>`;
+for (var i = 0; i < 45; i++) {
+  for (var j = 0; j < classItemArrays[i].length; j++) {
+    const item = classItemArrays[i][j];
+    itemcheckboxes += `
+    <div id="tmclass${i + 1}-${item}" class="fusion-form-checkbox">
+        <input
+          tabindex=""
+          id="checkbox-checkedItems-1-${i}-${item}"
+          type="checkbox"
+          value="tmclass${i + 1}-${item}"
+          name="checkedItems[]"
+          class="fusion-form-input"
+          data-holds-private-data="false"
+        />
+        <label for="checkbox-checkedItems-1-${i}-${item}"> ${item} </label>
+    </div>`;
+  }
+}
+
+itemcheckboxes += `</fieldset>`;
+document.write(itemcheckboxes);
