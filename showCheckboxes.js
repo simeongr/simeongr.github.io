14472f29f73ead5617ab4c536708da6fc15ed87a
Simeon Georgiev <simeonge@gmail.com>
2023-02-11 14:13:13 +0200

    WIP

--- showCheckboxes.js
+++ showCheckboxes.js
@@ -1,49 +1,57 @@
 // Lists all 45 TM classes as checkboxes
 function trimString(text, i) {
-    if (text.length < 100) {
-        return text;
-    }
+  if (text.length < 100) {
+    return text;
+  }
 
-    return `${text.substring(0, 100)}<span id="dots${i}">...</span><span id="more${i}" style="display:none">${text.substring(100, text.length)}</span>`;
+  return `${text.substring(
+    0,
+    100
+  )}<span id="dots${i}">...</span><span id="more${i}" style="display:none">${text.substring(
+    100,
+    text.length
+  )}</span>`;
 }
 
 function showMoreButton(text, i) {
-    if (text.length > 100) {
-        return `<a onclick="showMore(this, 'dots${i}', 'more${i}')">Покажи още</a>`;
-    }
+  if (text.length > 100) {
+    return `<a onclick="showMore(this, 'dots${i}', 'more${i}')">Покажи още</a>`;
+  }
 
-    return ''
+  return "";
 }
 
-export showCheckboxes function() {
-let checkboxes = `<fieldset>`
-for (var i = 0; i < 45; i++) {
+export function showCheckboxes() {
+  let checkboxes = `<fieldset>`;
+  for (var i = 0; i < 45; i++) {
     const classText = trimString(classDefinitions[i], i);
-    const moreButton = showMoreButton(classDefinitions[i], i)
+    const moreButton = showMoreButton(classDefinitions[i], i);
     checkboxes += `
-    <div id="tmclass${i+1}" class="fusion-form-checkbox">
+    <div id="tmclass${i + 1}" class="fusion-form-checkbox">
         <input
           tabindex=""
           id="checkbox-checkedclasses-1-${i}"
           type="checkbox"
-          value="tmclass${i+1}"
+          value="tmclass${i + 1}"
           name="checkedclasses[]"
           class="fusion-form-input"
           data-holds-private-data="false"
         />
         <label for="checkbox-checkedclasses-1-${i}"> ${classText} </label>
         ${moreButton}
-    </div>`
-}
-checkboxes+=`</fieldset>`
-document.write(checkboxes);
+    </div>`;
+  }
+  checkboxes += `</fieldset>`;
+  document.write(checkboxes);
 
-let itemcheckboxes = ""
-for (var i = 0; i < 45; i++) {
-    itemcheckboxes += `<fieldset id="classItem-${i}" hidden="hidden"> <legend>Клас ${i+1}</legend>`;
-  for (var j = 0; j < classItemArrays[i].length; j++) {
-    const item = classItemArrays[i][j];
-    itemcheckboxes += `
+  let itemcheckboxes = "";
+  for (var i = 0; i < 45; i++) {
+    itemcheckboxes += `<fieldset id="classItem-${i}" hidden="hidden"> <legend>Клас ${
+      i + 1
+    }</legend>`;
+    for (var j = 0; j < classItemArrays[i].length; j++) {
+      const item = classItemArrays[i][j];
+      itemcheckboxes += `
     <div id="tmclass${i + 1}-${item}" class="fusion-form-checkbox">
         <input
           tabindex=""
@@ -56,9 +64,9 @@ for (var i = 0; i < 45; i++) {
         />
         <label for="checkbox-checkedItems-1-${i}-${item}"> ${item} </label>
     </div>`;
+    }
+    itemcheckboxes += `</fieldset>`;
   }
-  itemcheckboxes += `</fieldset>`;
-}
 
-document.write(itemcheckboxes);
+  document.write(itemcheckboxes);
 }
