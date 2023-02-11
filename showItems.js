cecb5764d94b5e0df655c32cc5a80c05e907a130
Simeon Georgiev <simeonge@gmail.com>
2023-02-11 09:44:21 +0200

    WIP

--- showItems.js
+++ showItems.js
@@ -21,3 +21,18 @@ for (var i = 0; i < 45; i++) {
 }
 
 document.write(itemcheckboxes);
+
+for (var i = 0; i < 45; i++) {
+    const t = document.getElementById(checkboxIDs[i])
+    t.addEventListener("change", function(){
+      if (this.checked) {
+        document
+            .getElementById(`classItem-${i}`)
+            .removeAttribute("hidden");
+      } else {
+        document
+            .getElementById(`classItem-${i}`)
+            .setAttribute("hidden", "hidden");
+      }
+    })
+  }
