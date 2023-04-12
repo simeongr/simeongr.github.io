f0346f8abdf4d7c2ce13f2dfd451282f91124893
Simeon Georgiev <simeonge@gmail.com>
2023-04-12 16:30:52 +0300

    WIP

--- showItemsTest.js
+++ showItemsTest.js
@@ -1,10 +1,3 @@
-<div class="fusion-form-field fusion-form-text-field fusion-form-label-above" style="" data-form-id="1773">
-  <div class="fusion-form-input-with-icon">
-    <i class="awb-form-icon fa-search fas"></i>
-    <input type="text" name="filter" id="filter" value="" class="fusion-form-input" placeholder="Филтрирай продуктите по ключова дума..." data-holds-private-data="false" minlength="0">
-  </div>
-</div>
-
 let checkboxes = ``;
 for (var i = 0; i < 45; i++) {
   for (var j = 0; j < classItemArrays[i].length; j++) {
@@ -30,7 +23,6 @@ var container = document.getElementById("classItemsCheckboxesForm");
 var content = document.createElement("span");
 content.innerHTML = checkboxes;
 container.appendChild(content);
-// }
 
 let inputFilter = () => {
   let filterElementValue = document
