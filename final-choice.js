7bbcc6c15567feb4b327911654741abb5efbca1f
Simeon Georgiev <simeonge@gmail.com>
2023-04-15 22:18:24 +0300

    WIP

--- final-choice.js
+++ final-choice.js
@@ -1,10 +1,30 @@
 // Put this is Avada Code Block:
 // <div class="fusion-text fusion-text-2">
-//     <p id="choise" style="text-align: center;">
-//         Вие сте избрали следните стоки и услуги:
+//     <p id="choice" style="text-align: center;">
+//         Моля изберете стоки и услуги!
 //     </p>
 // </div>
 // <script "application/javascript" src="https://simeongr.github.io/final-choice.js"></script>
+
+setInterval(function() {
+    var hasChecked = false
+    for (var i = 0; i < 45; i++) {
+        for (var j = 0; j < classItemArrays[i].length; j++) {
+            if (document.getElementById(`tmclass${i}-item-${j}-final`).checked) {
+                var paragraph = document.getElementById("choice");
+                paragraph.innerHTML = "Вие сте избрали следните стоки и услуги:";
+                hasChecked = true;
+            }
+        }
+    }
+
+    if (!hasChecked) {
+        var paragraph = document.getElementById("choice");
+        paragraph.innerHTML = "Моля изберете стоки и услуги!";
+    }
+
+  }, 500);
+  
 
 document.write(`<fieldset id="classItemsCheckboxesForm-final"></fieldset>`)
 let checkboxesFinal = ``;
